da313fc3dc3f1f9151e9a8ff6cd4c5f895e30c22
Gerard Solanes <gerardsolher@gmail.com>
2023-10-12 16:21:49 +0200

    Adds loading before map renders

--- app/maps/page.tsx
+++ app/maps/page.tsx
@@ -4,12 +4,9 @@ import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const Map = () => {
-  interface Coordinates {
-    lat: number;
-    lon: number;
-  }
   const [map, setMap] = useState<mapboxgl.Map | undefined>(undefined);
   const mapNode = useRef(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const defaultLat = -70;
   const defaultLon = 42;
@@ -17,66 +14,73 @@ const Map = () => {
   const maxZoom = 10;
   const minZoom = 1;
 
-  useEffect(() => {
-    const node = mapNode.current;
-    if (typeof window === "undefined" || node === null) return;
-
+  const initializeMap = (
+    node: HTMLElement,
+    lon: number | null,
+    lat: number | null
+  ) => {
+    console.log("init map");
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN || "";
-
-    // Get user's geolocation
-    if (navigator.geolocation) {
-      console.log("in geoloc");
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          console.log(map);
-          const mapboxMap = new mapboxgl.Map({
-            container: node,
-            style: "mapbox://styles/quotropic/clmowfalm01zb01r42wn8dm1u",
-            center: [position.coords.longitude, position.coords.latitude],
-            zoom: initialZoom,
-            maxZoom: maxZoom,
-            minZoom: minZoom,
-          });
-
-          mapboxMap.on("load", () => {
-            setMap(mapboxMap);
-          });
-
-          return () => {
-            mapboxMap.remove();
-          };
-        },
-        (error) => {
-          console.error("Geolocation error:", error);
-          initializeMap(); // Fall back to using default coordinates
-        }
-      );
-    } else {
-      initializeMap(); // Fall back to using default coordinates
+    if (!lon || !lat) {
+      lon = defaultLon;
+      lat = defaultLat;
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const initializeMap = () => {
-    console.log("in init map");
-    const node = mapNode.current;
-    if (!node) return;
-
     const mapboxMap = new mapboxgl.Map({
       container: node,
       style: "mapbox://styles/quotropic/clmowfalm01zb01r42wn8dm1u",
-      center: [defaultLon, defaultLat],
+      center: [lon, lat],
       zoom: initialZoom,
       maxZoom: maxZoom,
       minZoom: minZoom,
     });
 
-    return () => {
-      mapboxMap.remove();
-    };
+    setMap(mapboxMap);
+    setIsLoading(false);
+    console.log("changed loading");
   };
 
-  return <div ref={mapNode} style={{ width: "100%", height: "400px" }} />;
+  useEffect(() => {
+    console.log("uesef");
+    const node = mapNode.current;
+    if (typeof window === "undefined" || node === null) return;
+    // Get user's geolocation
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          initializeMap(
+            node,
+            position.coords.longitude,
+            position.coords.latitude
+          );
+          return () => {
+            if (map) {
+              map.remove();
+            }
+          };
+        },
+        (error) => {
+          console.error("Geolocation error:", error);
+          initializeMap(node, null, null); // Fall back to using default coordinates
+        }
+      );
+    } else {
+      initializeMap(node, null, null); // Fall back to using default coordinates
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <div className="mapContainer">
+        {isLoading && <h2>Loading</h2>}
+        <div
+          ref={mapNode}
+          id="map"
+          style={{ width: "100%", height: "400px" }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Map;
